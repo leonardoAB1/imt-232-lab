875d0f5e9681278931f7369a2492e5b6bfd8dfc5
Leonardo Achá <leonardoacha@hotmail.com>
2023-02-13 09:38:41 -0400

    improved codumentation for main.h file

--- main.h
+++ main.h
@@ -1,10 +1,24 @@
 /**
-  ******************************************************************************
-  * @file		main.h
-  * @author	Leonardo Acha Boiano
-  * @date		09 Feb 2023
-  ******************************************************************************
-*/
+ * @file        main.h
+ * @author      Leonardo Acha Boiano
+ * @date        09 Feb 2023
+ * @brief       Embedded systems II: LAB 00
+ *
+ * This program sets the outputs of a Blue Pill development board based on inputs
+ * received from buttons connected to the board. The program sets three digital
+ * inputs, PA4, PA5, and PA6, with pull-up resistors and four outputs, PB13,
+ * PB14, PB15, and PA8(PWM). The inputs control the duty cycle of a PWM signal on PA8
+ * and the color of an RGB LED connected to the board.
+ *
+ * PA4: button_1: Set color red, duty cycle 0%
+ * PA5: button_2: Set color green, duty cycle 50%
+ * PA6: button_3: Set color blue, duty cycle 100%
+ *
+ * @note        This code is written in C and is used on a Blue Pill development board.
+ *
+ * @copyright   Copyright (c) 2023 by Leonardo Acha Boiano. All rights reserved.
+ *
+ */
 
 #ifndef __MAIN_H
 #define __MAIN_H
