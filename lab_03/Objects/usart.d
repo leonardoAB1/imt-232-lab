f8f6218298d4cf9c1dc2d672c2a517e7d9b96410
LEONARDO ACHA <leonardo.acha@ucb.edu.bo>
2023-03-26 18:00:41 -0400

    Usart driver added

---
+++ lab_03/Objects/usart.d
@@ -0,0 +1,10 @@
+./objects/usart.o: usart.c usart.h \
+  C:\Users\ZEPHYRUS\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.4.0\Device\Include\stm32f10x.h \
+  RTE\_Target_1\RTE_Components.h \
+  C:\Users\ZEPHYRUS\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm3.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Users\ZEPHYRUS\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\ZEPHYRUS\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\ZEPHYRUS\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Users\ZEPHYRUS\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.4.0\Device\Include\system_stm32f10x.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h
